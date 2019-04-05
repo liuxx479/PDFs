5d076018986f5e22e4188684ded179b702485fb1
liuxx479 <liuxx479@gmail.com>
2019-04-05 13:42:40 -0400

    redo pdf with correct sigma_kappa

--- pdf_theory.py
+++ pdf_theory.py
@@ -25,43 +25,32 @@ z_arr = concatenate([arange(0.5, 3, 0.5),[1100.,]])
 sigmakappa_arr = array([ [std(smooth(mapgen(iz, 5, 0), thetaG).flatten()) 
                  for thetaG in thetaG_arr] for iz in z_arr])
 
-sigmakappa_arr_massive = array([[ 0.00459626,  0.00308772,  0.00220381],
-       [ 0.00708774,  0.00497572,  0.003762  ],
-       [ 0.00927037,  0.00648126,  0.00492705],
-       [ 0.01086472,  0.00743865,  0.00551859],
-       [ 0.01226901,  0.00834649,  0.00608883],
-       [ 0.03019188,  0.01931345,  0.01356282]])
+sigmakappa_arr_massive = array([[0.00356207, 0.00249696, 0.00195413],
+       [0.0067694 , 0.00477232, 0.00374541],
+       [0.00935179, 0.00661128, 0.00517522],
+       [0.01143028, 0.00807851, 0.0063025 ],
+       [0.01312191, 0.00925983, 0.00720139],
+       [0.03459323, 0.02328923, 0.01740048]])
 
 
-sigmakappa_arr = array([[ 0.0047579 ,  0.00320448,  0.0022913 ],
-       [ 0.00734598,  0.0051601 ,  0.00390173],
-       [ 0.00954841,  0.00667598,  0.00507506],
-       [ 0.01116931,  0.00764257,  0.00567065],
-       [ 0.01260201,  0.00856384,  0.00624893],
-       [ 0.03081554,  0.01968398,  0.01379884]])
-
-correct_ratio = array([[0.7652519052760128, 0.8536888729600138, 0.98560815],
-       [0.97673189, 1.01999364, 1.09682727],
-       [1.0377963 , 1.08292562, 1.15041094],
-       [1.08185203, 1.14821166, 1.24154902],
-       [1.0987346 , 1.16937103, 1.27900662],
-       [1.16195681, 1.24064477, 1.34414003]])
-
-sigmakappa_arr*=correct_ratio
+sigmakappa_arr = array([[0.00368035, 0.00257658, 0.00201411],
+       [0.00697669, 0.00490864, 0.00384789],
+       [0.00962323, 0.00678943, 0.00530901],
+       [0.01175002, 0.0082883 , 0.0064598 ],
+       [0.01347936, 0.0094942 , 0.00737679],
+       [0.03531527, 0.02374736, 0.01773231]])
 
 ##### pre-defined bin edges, 100 linear bins between -5, 5 sigma_kappa
 binedges_fun = lambda sigmak: np.linspace(-5*sigmak, 5*sigmak, 101)
 binedges = array([map(binedges_fun,isigmakappa_arr) for isigmakappa_arr in sigmakappa_arr])
 
 #### smooth function, for all smoothing bins
-#def smooth_map (r, mnu=imnu, zidx=izidx):
-    #imap = mapgen(z_arr[zidx], r, mnu)
-    #imap_smooth = array([smooth(imap, thetaG) for thetaG in thetaG_arr])
-    #hist_arr = array([histogram(imap_smooth[i], bins=binedges[zidx, i])[0] 
-                      #for i in range(len(thetaG_arr))])
-    #std_arr = std(imap_smooth,axis=1)
-    ##print hist_arr.shape, std_arr.shape
-    #return hist_arr, std_arr[0], std_arr[1], std_arr[2]
+def smooth_map (r, mnu=imnu, zidx=izidx):
+    imap = mapgen(z_arr[zidx], r, mnu)
+    imap_smooth = array([smooth(imap, thetaG) for thetaG in thetaG_arr])
+    hist_arr = array([histogram(imap_smooth[i], bins=binedges[zidx, i])[0] 
+                      for i in range(len(thetaG_arr))])
+    return hist_arr
 
 def std_map (r, mnu=imnu, zidx=izidx):
     imap = mapgen(z_arr[zidx], r, mnu)
@@ -75,17 +64,13 @@ if not pool.is_master():
     sys.exit(0)
 
 print 'Mnu, z:', imnu, z_arr[izidx]
-#out = array(pool.map(smooth_map, range(1,10001)))
-out_std = array(pool.map(std_map, range(1,10001)))
-#for j in range(len(thetaG_arr)):
-    #print out.shape
-    #save(out_dir+'PDFs_Mnu0.%i_z%.1f_smooth%02d.npy'%(imnu, z_arr[izidx], thetaG_arcmin[j]), out[:,j,:])
-print out_std.shape
-save(out_dir+'PDFs_Mnu0.%i_z%.1f.npy'%(imnu, z_arr[izidx]), out_std)
+out = array(pool.map(smooth_map, range(1,10001)))
+for j in range(len(thetaG_arr)):
+    print out.shape
+    save(out_dir+'PDFs_Mnu0.%i_z%.1f_smooth%02d.npy'%(imnu, z_arr[izidx], thetaG_arcmin[j]), out[:,j,:])
+#out_std = array(pool.map(std_map, range(1,10001)))
+#print out_std.shape
+#save(out_dir+'PDFs_Mnu0.%i_z%.1f.npy'%(imnu, z_arr[izidx]), out_std)
 
 pool.close()
-
-#out_massless = array(pool.map(lambda p: smooth_map(p, mnu=1), range(1,10000)))
-### output shape=(9999, 3, 100) for 9999 realizations, 3 smoothing scale, 100 bins
-
 print 'DONE DONE DONE'
