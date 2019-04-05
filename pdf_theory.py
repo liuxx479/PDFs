7222a4d3f69ec746cc077dc434245316b8d6f6e3
liuxx479 <liuxx479@gmail.com>
2019-04-05 12:28:56 -0400

    correct for sigma_kappa

--- pdf_theory.py
+++ pdf_theory.py
@@ -60,6 +60,7 @@ def smooth_map (r, mnu=imnu, zidx=izidx):
     hist_arr = array([histogram(imap_smooth[i], bins=binedges[zidx, i])[0] 
                       for i in range(len(thetaG_arr))])
     std_arr = array([std(imap) for imap in imap_smooth])
+    print hist_arr.shape, std_arr.shape
     return hist_arr, std_arr
     
 pool=MPIPool()
